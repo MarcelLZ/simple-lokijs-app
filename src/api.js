9ec0cb5d59aef09afb5725079168210237e4aafa
Marcel Zanluca <marcel.zanluca@gmail.com>
2017-10-20 00:10:24 -0200

    correcting http method from /clear

--- src/api.js
+++ src/api.js
@@ -4,17 +4,16 @@ import { save, getAll, clearDb } from './model'
 const router = new Router()
 
 router.post('/', (request, response) => {
-  let { description } = request.body;
+  let { description } = request.body
   save({ description })
 
   response.json(description)
 })
 
-router.get('/clear-db',(request,response) =>{
-   clearDb()
-  response.sendStatus(204);
-});
-
+router.post('/clear', (request, response) => {
+  clearDb()
+  response.sendStatus(204)
+})
 
 router.get('/', (request, response) => response.json(getAll()))
 
